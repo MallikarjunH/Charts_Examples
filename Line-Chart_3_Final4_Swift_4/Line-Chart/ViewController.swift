66d0e6a198446b0edeea2fb8c215b943427592b3
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-08-05 12:46:30 +0530

    Added Swift 4 verion example
     chartViewOutlet.xAxis.valueFormatter = IndexAxisValueFormatter(values: months)      -> this line/code is available in after swift 4.2, swift 5 ....sol solution for this custome class is added in order to show values on x axis.

--- Line-Chart_3_Final4_Swift_4/Line-Chart/ViewController.swift
+++ Line-Chart_3_Final4_Swift_4/Line-Chart/ViewController.swift
@@ -43,46 +43,11 @@ class ViewController: UIViewController {
         
         
         
-        let lineChartDataSet = LineChartDataSet(entries: dataEntries, label: "Units Consumed")
-      
-         // to show points (circle)
-        /*  lineChartDataSet.axisDependency = .left
-        lineChartDataSet.setColor(UIColor.black)
-        lineChartDataSet.setCircleColor(UIColor.black) // our circle will be dark red
-        lineChartDataSet.lineWidth = 1.0
-        lineChartDataSet.circleRadius = 3.0 // the radius of the node circle
-        lineChartDataSet.fillAlpha = 1
-        lineChartDataSet.fillColor = UIColor.black
-        lineChartDataSet.highlightColor = UIColor.white
-        lineChartDataSet.drawCircleHoleEnabled = true */
-       
-        /*
-           1. lineChartDataSet.setCircleColor(UIColor.clear) == the circle will be clear color with
-           2. If we we comment following line, by defaukt it will take blue color for that points (circle)
-         */
+        let lineChartDataSet = LineChartDataSet(values: dataEntries, label: "Units Consumed")
+    
         lineChartDataSet.setCircleColor(UIColor.clear) // hide the outer circle color
-        
-        /*
-         1. lineChartDataSet.circleRadius = 3.0 == it is the radius of the node circle
-         2. If we we comment following line, by default it will take following
-            lineChartDataSet.circleRadius = 3.0
-         3. If you wans to hide this also (cubic circle) then write following line
-             lineChartDataSet.circleRadius = 0.0
-         */
         lineChartDataSet.circleRadius = 0.0 // hide - the radius of the node circle
-        
-        /* Increasing/decresing the width of a line - curver line
-           by default its width will be 1.0 i.e
-          lineChartDataSet.lineWidth = 1.0
-         
-          If you wants to increse then just change like 2.0, 3.0 ...etc
-         */
         lineChartDataSet.lineWidth = 2.0 //1.0
-        
-        /*
-         -  show or hide values on the graph - in the lines - i.e points values by following line
-         - if you wants to show the values then just hide the below line - change or add custome color according to your requirement
-        */
         lineChartDataSet.valueTextColor = UIColor.clear //hide the values on the curve line
         
 
@@ -90,13 +55,6 @@ class ViewController: UIViewController {
         lineChartDataSet.colors = [NSUIColor.blue] //Sets the line colour to blue
         lineChartDataSet.mode = .cubicBezier
         lineChartDataSet.cubicIntensity = 0.2
-        
-        //NoteL: If you need show/add gradient color then enable following code
-    
-     /*   let gradient = getGradientFilling()
-        lineChartDataSet.fill = Fill.fillWithLinearGradient(gradient, angle: 90.0)
-        lineChartDataSet.drawFilledEnabled = false //hide filled color */
-        
         
         var dataSets = [LineChartDataSet]() //This is the object that will be added to the chart
         dataSets.append(lineChartDataSet)  //Adds the line to the dataSet
@@ -106,7 +64,7 @@ class ViewController: UIViewController {
         let lineChartData = LineChartData(dataSets: dataSets)
         chartViewOutlet.data = lineChartData //finally - it adds the chart data to the chart and causes an update
         
-        //cell.chartViewOutlet.chartDescription?.text = "" //hide descriptionn label //3.0 version required
+        chartViewOutlet.chartDescription?.text = "" //hide descriptionn label //3.0 version required
         
         chartViewOutlet.xAxis.enabled = true //show x axis
         chartViewOutlet.leftAxis.enabled = true //show/hide left axix (Y axis)
@@ -119,13 +77,8 @@ class ViewController: UIViewController {
         chartViewOutlet.xAxis.drawAxisLineEnabled = true //show x axis line
         chartViewOutlet.xAxis.labelPosition = .bottom // values/labels of x axis - position
         chartViewOutlet.xAxis.drawLabelsEnabled = true //show/hide values/labels in x axis
-        
-        //right and left axis - optional/not required
-        // chartViewOutlet.leftAxis.drawAxisLineEnabled = true //show lines of left x axis
+    
         chartViewOutlet.leftAxis.drawGridLinesEnabled = true //hide/show x axis grid lines - horizontal lines ( X axis - from Y)
-        //  chartViewOutlet.rightAxis.drawAxisLineEnabled = false
-        // chartViewOutlet.rightAxis.drawGridLinesEnabled = false
-        
        chartViewOutlet.legend.enabled = false //show.hide legend - below graph
         
         
@@ -135,7 +88,7 @@ class ViewController: UIViewController {
         let llXAxis = ChartLimitLine(limit: 10, label: "Index 10")
         llXAxis.lineWidth = 1.5
         llXAxis.lineDashLengths = [10, 10, 0]
-        llXAxis.labelPosition = .bottomRight
+        llXAxis.labelPosition = .rightBottom
         llXAxis.valueFont = .systemFont(ofSize: 10)
         
         chartViewOutlet.xAxis.gridLineDashLengths = [4, 4] // verticle line - dash lines
@@ -145,14 +98,14 @@ class ViewController: UIViewController {
         ll1.lineWidth = 1.5
         ll1.lineColor = UIColor.red
         ll1.lineDashLengths = [4, 4]
-        ll1.labelPosition = .topRight
+        ll1.labelPosition = .rightTop
         ll1.valueFont = .systemFont(ofSize: 10)
         
         let ll2 = ChartLimitLine(limit: 50, label: "Lower Limit") // limit the lower line
         ll2.lineWidth = 1.5
         ll2.lineColor = UIColor.green
         ll2.lineDashLengths = [4,4]
-        ll2.labelPosition = .bottomRight
+        ll2.labelPosition = .rightBottom
         ll2.valueFont = .systemFont(ofSize: 10)
         
         let leftAxis = chartViewOutlet.leftAxis
@@ -164,25 +117,43 @@ class ViewController: UIViewController {
         
         leftAxis.gridLineDashLengths = [5, 5] //horizontal line - dash lines
         leftAxis.drawLimitLinesBehindDataEnabled = true
+       
         
-        chartViewOutlet.xAxis.valueFormatter = IndexAxisValueFormatter(values: dataPoints) //enable/show months label at x axis
+        // swift 4.0 and lower versions
+        let customFormater = CustomFormatter()
+        customFormater.labels =  months
+        chartViewOutlet.xAxis.valueFormatter = customFormater
+      
+        // this availabele after swift 4.2 or 5.0
+       // chartViewOutlet.xAxis.valueFormatter = IndexAxisValueFormatter(values: months) //enable/show months label at x axis
         
         
     }
-
-    /// Creating gradient for filling space under the line chart
- /*  private func getGradientFilling() -> CGGradient {
-        // Setting fill gradient color
-        let coloTop = UIColor(red: 141/255, green: 133/255, blue: 220/255, alpha: 1).cgColor
-        let colorBottom = UIColor(red: 230/255, green: 155/255, blue: 210/255, alpha: 1).cgColor
-        // Colors of the gradient
-        let gradientColors = [coloTop, colorBottom] as CFArray
-        // Positioning of the gradient
-        let colorLocations: [CGFloat] = [0.7, 0.0]
-        // Gradient Object
-        return CGGradient.init(colorsSpace: CGColorSpaceCreateDeviceRGB(), colors: gradientColors, locations: colorLocations)!
-    } */
     
 
 }
 
+
+final class CustomFormatter: IAxisValueFormatter {
+    
+    var labels: [String] = []
+    
+    func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+        
+        let count = self.labels.count
+        
+        guard let axis = axis, count > 0 else {
+            return ""
+        }
+        
+        let factor = axis.axisMaximum / Double(count)
+        
+        let index = Int((value / factor).rounded())
+        
+        if index >= 0 && index < count {
+            return self.labels[index]
+        }
+        
+        return ""
+    }
+}
